123360f5271e510b628f6f501defa98f92e5329c
GFiirst <geovanefiirst@hotmail.com>
2026-04-14 09:27:05 -0400

    feat: configuring db in appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE! as 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+    })
+  ],
   controllers: [],
   providers: [],
 })
