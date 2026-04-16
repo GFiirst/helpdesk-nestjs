905a3abeac0fb3e8551433d3c16cf98b222fa04e
GFiirst <geovanefiirst@hotmail.com>
2026-04-16 15:22:48 -0400

    feat: create permissions entity

---
+++ src/permissions/permissions.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, ManyToMany } from "typeorm";
+import { Action } from "src/casl/enums/casl-action";
+import { Subjects } from "src/casl/enums/casl-subject";
+import { Roles } from "src/roles/roles.entity";
+
+@Entity()
+export class Permissions extends BaseEntity{
+    @Column({
+        type: "enum",
+        enum: Action
+    })
+    action: Action;
+
+    @Column()
+    subject: Subjects;
+
+    @ManyToMany(() => Roles, (roles) => roles.permissions)
+    roles: Roles[];
+}
