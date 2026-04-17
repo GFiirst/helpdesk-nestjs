6b171348b173803d71f43eb863dae23f4542b417
GFiirst <geovanefiirst@hotmail.com>
2026-04-17 15:41:11 -0400

    feat: add entities to entities

--- src/database/entities.ts
+++ src/database/entities.ts
@@ -1,5 +1,13 @@
+import { Roles } from "src/roles/roles.entity";
+import { RefreshToken } from "src/token/refresh-token.entity";
+import { User } from "src/users/users.entity";
 import { BaseEntity } from "typeorm";
 
 export const entities = [
-    BaseEntity
+    BaseEntity,
+    User,
+    Roles,
+    Permissions,
+    RefreshToken,
+
 ]
