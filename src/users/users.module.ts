2d75336bb646b77522dfb564f2866a84ad5af04e
GFiirst <geovanefiirst@hotmail.com>
2026-04-17 17:02:14 -0400

    creat: users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users.entity";
+import { Roles } from "src/roles/roles.entity";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([User, Roles])],
+    controllers:[UsersController],
+    providers:[UsersService],
+    exports:[UsersService]
+})
+export class UsersModule{}
