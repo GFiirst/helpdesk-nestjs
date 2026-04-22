870d4a8f60e875d9b3f4d8ee78d550b3fc6a72af
GFiirst <geovanefiirst@hotmail.com>
2026-04-22 10:53:23 -0400

    feat: create authguard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,53 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
+import { UsersService } from "src/users/users.service";
+import { IS_PUBLIC_KEY } from "./decorators/is-public.decorator";
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+    constructor(
+        private jwtService: JwtService,
+        private reflector: Reflector,
+        private readonly UsersService: UsersService
+    ){}
+
+    async canActivate(context: ExecutionContext){
+        const req = context.switchToHttp().getRequest()
+        const token = this.extractTokenFromHeader(req)
+
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if(isPublic) {
+            return true;
+        }
+
+        if(!token) {
+            throw new UnauthorizedException()
+        }
+        try {
+            const payload = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: process.env.JWT_SECRET
+                }
+            );
+
+            const user = await this.UsersService.findByIdWithRolesAndPermissions(payload.sub)
+
+            req['user'] = user;
+        } catch(err) {
+            throw new UnauthorizedException();
+        }
+        return true;
+
+    }
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
+}
