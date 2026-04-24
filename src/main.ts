7360a5f8df7453ff0a25e3140e31a62964d9736b
GFiirst <geovanefiirst@hotmail.com>
2026-04-24 14:46:53 -0400

    feat: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.ORIGIN!
   });
+
+  if(process.env.ENV === 'dev'){
+    const config = new DocumentBuilder()
+      .setTitle('Helpdesk API')
+      .setDescription('Helpdesk API documentation')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, documentFactory);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
