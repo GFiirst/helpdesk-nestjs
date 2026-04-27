3110ba0a792a3cd67c43c886429b0d25ef3cdcbc
GFiirst <geovanefiirst@hotmail.com>
2026-04-27 11:30:05 -0400

    feat: implement createUser service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./users.entity";
 import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import * as bcrypt from "bcrypt";
+import { Roles } from "src/roles/roles.entity";
+import { UserRoles } from "src/roles/enums/user-roles";
 
 @Injectable()
 export class UsersService {
@@ -9,19 +13,44 @@ export class UsersService {
     constructor(
         @InjectRepository(User)
         private userRepository: Repository<User>,
+        @InjectRepository(Roles)
+        private rolesRepository: Repository<Roles>,
     ){}
 
-    async findByIdWithRolesAndPermissions(id: string) {
-        const user = await this.userRepository.findOne({
-            where: {
-                id: id
+    async createUser(createUser: CreateUserDto) {
+        const newUser = new User();
+
+        const existingEmail = await this.userRepository.findOne({ 
+            where: { 
+                email: createUser.email 
+            } 
+        });
+
+        if (existingEmail) {
+            throw new BadRequestException("Email already exists");
+        }
+
+        const userPass = createUser.password;
+        const criptPass = await bcrypt.hash(userPass, +process.env.BCRYPT_SALT!);
+
+        newUser.name = createUser.name;
+        newUser.email = createUser.email;
+        newUser.password = criptPass;
+        
+        const userRole = await this.rolesRepository.findOne({
+            where:{
+                role: UserRoles.USER
             },
-            relations: {
-                roles: {
-                    permissions: true
-                }
-            }
         })
-        return user
+
+        if(!userRole){
+            throw new BadRequestException("User role not found");
+        }
+        newUser.roles = [userRole];
+
+        await this.userRepository.save(newUser);
+        return{
+            message: "User created successfully"
+        }
     }
 }
