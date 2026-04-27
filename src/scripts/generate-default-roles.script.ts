1dc275bdc1a5d99e74d483392431f431f126affa
GFiirst <geovanefiirst@hotmail.com>
2026-04-27 14:41:52 -0400

    feat: create script to generate default roles

---
+++ src/scripts/generate-default-roles.script.ts
@@ -0,0 +1,45 @@
+import { generate } from "rxjs";
+import { AppDataSource } from "src/database/data-source";
+import { UserRoles } from "src/roles/enums/user-roles";
+import { Roles } from "src/roles/roles.entity";
+
+const rolesAlreadyCreated: string[] = [];
+
+async function generateDefaultRoles() {
+    await AppDataSource.initialize();
+
+    const roleRepository = AppDataSource.getRepository(Roles);
+
+    const rolesValues = Object.values(UserRoles);
+
+    for (const role of rolesValues) {
+        const newRole = new Roles();
+        newRole.role = role as UserRoles;
+
+        const exRole = await roleRepository.findOne({
+            where: {
+                role: role as UserRoles
+            }
+        });
+
+        if (!exRole) {
+            rolesAlreadyCreated.push(role);
+            continue
+        }
+
+        await roleRepository.save(newRole);
+    }
+}
+
+generateDefaultRoles()
+    .then(()=>{
+        console.log("Default roles created successfully");
+        console.log("Roles already created: " + rolesAlreadyCreated);
+    })
+    .catch((error)=>{
+        console.error("Error creating default roles: ");
+        console.error(error);
+    })
+    .finally(()=>{
+        AppDataSource.destroy();
+    })
