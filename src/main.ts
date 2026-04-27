0493c5f1fc7f360c40c66c3512bf884827bd3a68
GFiirst <geovanefiirst@hotmail.com>
2026-04-27 10:07:13 -0400

    fix: configure cookieparser

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+  import { ValidationPipe } from '@nestjs/common';
+  import cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.ORIGIN!
-  });
 
-  if(process.env.ENV === 'dev'){
-    const config = new DocumentBuilder()
-      .setTitle('Helpdesk API')
-      .setDescription('Helpdesk API documentation')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: process.env.ORIGIN!,
+      credentials: true
+    });
 
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, documentFactory);
+    if(process.env.ENV === 'dev'){
+      const config = new DocumentBuilder()
+        .setTitle('Helpdesk API')
+        .setDescription('Helpdesk API documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+      const documentFactory = () => SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('docs', app, documentFactory);
+    }
+
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.PORT ?? 3000);
   }
-
-  app.use(CookieParser());
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  bootstrap();
