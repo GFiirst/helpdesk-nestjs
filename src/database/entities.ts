e063d6b48cec3ad73acddd7532e4ae38b692a730
GFiirst <geovanefiirst@hotmail.com>
2026-05-28 09:16:22 -0400

    feat: add profile and user entitie to entities

--- src/database/entities.ts
+++ src/database/entities.ts
@@ -1,11 +1,14 @@
+import { Credential } from "src/auth/entities/credential.entity";
 import { Roles } from "src/roles/roles.entity";
 import { RefreshToken } from "src/auth/entities/refresh-token.entity";
-import { BaseEntity } from "typeorm";
+import { User } from "src/users/entity/users.entity";
+import { Profile } from "src/users/entity/profile.entity";
 import { Permissions } from "src/permissions/permissions.entity";
 
 export const entities = [
-    BaseEntity,
     Credential,
+    User,
+    Profile,
     Roles,
     Permissions,
     RefreshToken,
