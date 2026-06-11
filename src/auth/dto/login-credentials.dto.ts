dceeef80b9f13fc7e99c22f10762d7b0f4adade1
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-06-11 10:21:13 -0400

    feat: add body example for swagger

--- src/auth/dto/login-credentials.dto.ts
+++ src/auth/dto/login-credentials.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, MaxLength } from "class-validator";
 
 export class LoginCredentialsDto {
     @IsEmail()
     @IsNotEmpty()
     @MaxLength(255, { message: "Email must be at most 255 characters long" })
+    @ApiProperty({example: "admin@gmail.com"})
     email: string;
 
     @IsString()
     @IsNotEmpty()
     @MaxLength(255, { message: "Password must be at most 255 characters long" })
+    @ApiProperty({example: "123"})
     password: string;
 }
